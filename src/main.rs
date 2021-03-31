59cedeac070ddbb177babc8f0b36d13de9d4cb2b
Eric Mikulin <ericm99@gmail.com>
2021-03-31 01:12:28 -0700

    Add latest route

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ extern crate uuid;
 extern crate warp;
 
 use clokwerk::{Scheduler, TimeUnits};
+use std::fs;
+use std::io;
 use std::process::{Command, Stdio};
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use warp::{http::Response, Filter};
+
+const IMAGE_DIR: &str = "images/";
 
 fn base_camera_command() -> std::process::Command {
     let mut command = Command::new("raspistill");
@@ -26,6 +30,17 @@ fn take_and_save_image(filepath: String) {
     let _ = child.wait().unwrap();
 }
 
+fn take_image() -> Vec<u8> {
+    let mut command = base_camera_command();
+    command.arg("-o");
+    command.arg("-");
+    command.stdout(Stdio::piped());
+
+    let child = command.spawn().expect("Command failed to start");
+    let output = child.wait_with_output().unwrap();
+    return output.stdout;
+}
+
 fn gen_timelapse_filename() -> u64 {
     let start = SystemTime::now();
     let since_the_epoch = start.duration_since(UNIX_EPOCH).unwrap();
@@ -34,7 +49,7 @@ fn gen_timelapse_filename() -> u64 {
 
 fn take_timelapse_photo() {
     let filename = gen_timelapse_filename();
-    let filepath = format!("images/{}.jpg", filename);
+    let filepath = format!("{}{}.jpg", IMAGE_DIR, filename);
 
     take_and_save_image(filepath);
 }
@@ -50,32 +65,44 @@ fn timelapse_thread() {
     }
 }
 
-fn take_image() -> Vec<u8> {
-    let mut command = base_camera_command();
-    command.arg("-o");
-    command.arg("-");
-    command.stdout(Stdio::piped());
+fn image_response(image: Vec<u8>) -> Result<Response<Vec<u8>>, warp::http::Error> {
+    return Response::builder()
+        .header("Content-Type", "image/jpeg")
+        .body(image);
+}
 
-    let child = command.spawn().expect("Command failed to start");
-    let output = child.wait_with_output().unwrap();
-    return output.stdout;
+fn handle_latest() -> Result<Response<Vec<u8>>, warp::http::Error> {
+    let mut entries = fs::read_dir(IMAGE_DIR)
+        .unwrap()
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()
+        .unwrap();
+    entries.sort();
+    println!("{:?}", entries);
+    println!("{:?}", &entries[0]);
+
+    let filepath = &entries[0];
+    let image = fs::read(filepath).unwrap();
+
+    return image_response(image);
 }
 
 fn handle_current() -> Result<Response<Vec<u8>>, warp::http::Error> {
     let image = take_image();
-    return Response::builder()
-        .header("Content-Type", "image/jpeg")
-        .body(image);
+    return image_response(image);
 }
 
 #[tokio::main]
 async fn main() {
     let timelapse_child = thread::spawn(timelapse_thread);
 
-    let index = warp::get().and(warp::path::end()).map(|| handle_current());
-    let current = warp::path("current").and(warp::path::end()).map(|| handle_current()).with(warp::compression::gzip());
+    let index = warp::get().and(warp::path::end()).map(|| handle_latest());
+    let current = warp::path("current")
+        .and(warp::path::end())
+        .map(|| handle_current());
+    let images = warp::path("images").and(warp::fs::dir(IMAGE_DIR));
 
-    let routes = index.or(current);
+    let routes = index.or(current).or(images);
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 
     timelapse_child.join().unwrap();

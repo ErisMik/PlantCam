aa54564f928dde0e5713c77e8fd7eb7aca6df65e
Eric Mikulin <ericm99@gmail.com>
2021-03-31 00:41:49 -0700

    Add compression

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ extern crate uuid;
 extern crate warp;
 
 use clokwerk::{Scheduler, TimeUnits};
-use std::fs;
 use std::process::{Command, Stdio};
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use uuid::Uuid;
 use warp::{http::Response, Filter};
 
 fn base_camera_command() -> std::process::Command {
@@ -26,17 +24,6 @@ fn take_and_save_image(filepath: String) {
 
     let mut child = command.spawn().expect("Command failed to start");
     let _ = child.wait().unwrap();
-}
-
-fn take_image() -> Vec<u8> {
-    let mut command = base_camera_command();
-    command.arg("-o");
-    command.arg("-");
-    command.stdout(Stdio::piped());
-
-    let child = command.spawn().expect("Command failed to start");
-    let output = child.wait_with_output().unwrap();
-    return output.stdout;
 }
 
 fn gen_timelapse_filename() -> u64 {
@@ -63,32 +50,30 @@ fn timelapse_thread() {
     }
 }
 
-fn take_instant_photo() -> Vec<u8> {
-    let filename = Uuid::new_v4().to_hyphenated().to_string();
-    let filepath = format!("/tmp/{}.jpg", filename);
+fn take_image() -> Vec<u8> {
+    let mut command = base_camera_command();
+    command.arg("-o");
+    command.arg("-");
+    command.stdout(Stdio::piped());
 
-    take_and_save_image(filepath.clone());
+    let child = command.spawn().expect("Command failed to start");
+    let output = child.wait_with_output().unwrap();
+    return output.stdout;
+}
 
-    let image = fs::read(filepath).unwrap();
-    return image;
+fn handle_current() -> Result<Response<Vec<u8>>, warp::http::Error> {
+    let image = take_image();
+    return Response::builder()
+        .header("Content-Type", "image/jpeg")
+        .body(image);
 }
 
 #[tokio::main]
 async fn main() {
     let timelapse_child = thread::spawn(timelapse_thread);
 
-    let index = warp::get().and(warp::path::end()).map(|| {
-        let image = take_instant_photo();
-        return Response::builder()
-            .header("Content-Type", "image/jpeg")
-            .body(image);
-    });
-    let current = warp::path("current").and(warp::path::end()).map(|| {
-        let image = take_image();
-        return Response::builder()
-            .header("Content-Type", "image/jpeg")
-            .body(image);
-    });
+    let index = warp::get().and(warp::path::end()).map(|| handle_current());
+    let current = warp::path("current").and(warp::path::end()).map(|| handle_current()).with(warp::compression::gzip());
 
     let routes = index.or(current);
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
